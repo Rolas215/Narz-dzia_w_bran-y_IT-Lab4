11678359d423bb5af0e5f3f86ced60946cea64ee
Rolas215 <fabian.jasiak@onet.eu>
2024-06-04 17:04:22 +0200

    Add --logs option

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 case $1 in
-	--date|-d)
+	--date)
 		echo "Today's date is: $(date)"
+		;;
+	--logs)
+		if [ -z "$2" ]; then
+			num=100
+		else
+			num=$2
+		fi
+		for ((i=1; i<=num; i++))
+		do
+			filename="log$i.txt"
+			echo "File: $filename, Created by: $0, Date: $(date)" > $filename
+		done
 		;;
 	*)
 		echo "Invalid option. Use --help or -h for help."
