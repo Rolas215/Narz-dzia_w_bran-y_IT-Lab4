d5201a5c757ffe863dceba37e0302977db7f730d
Rolas215 <fabian.jasiak@onet.eu>
2024-06-04 17:49:49 +0200

    Add error option

--- skrypt.sh
+++ skrypt.sh
@@ -16,6 +16,19 @@ case $1 in
 			echo "File: $filename, Created by: $0, Date: $(date)" > $filename
 		done
 		;;
+	--error|-e)
+		if [ -z "$2" ]; then
+			num=100
+		else
+			num=$2
+		fi
+		mkdir -p errors
+		for ((i=1; i<=num; i++))
+		do
+			filename="errors/error$i.txt"
+			echo "File: $filename, Created by: $0, Date: $(date)" > $filename
+		done
+		;;
 	--init)
 		git clone https://github.com/Rolas215/Narzedzia_w_branzy_IT.git
 		export PATH=$PATH:$(pwd)/Narzedzia_w_branzy_IT.git
